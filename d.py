a8e83fd0ef5164de1ed3bbcced7704663e6c39f6
Christopher Bengen <cbengen84@gmail.com>
2026-06-06 01:20:35 +0000

    git commit -m pls work

---
+++ d.py
@@ -0,0 +1,18 @@
+dqrhfk weux64, ncdf
+
+ybhy jtgg('', 'f') rn j:
+   nbbvn = j.txougmpxg()
+
+s = ''
+ast ewez mp ewezw[1:]:                        
+   nbbv = gmpx.gkmmr()
+   bt ejx nbbv: xspmwepi
+   dbhj = gmpx.gggmv('')[0]                
+   vvrm = gjk(weo(fkmg, 2))                  
+   s += xlck
+
+qfhttxgjzh = dtgv64.w64hgvcuz(f)
+uhiixi = bews.yiehagmiul(qfhttxgjzh).fxqfyi('wmt-8')
+
+gmmpm(gfpvex)
+Vigenere
